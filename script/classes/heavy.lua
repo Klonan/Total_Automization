267a587ed681dfd4ea703cd21fbfc5880699336b
Klonan <konranwoodhouse@hotmail.com>
2018-06-14 15:19:22 +0200

    Minigun

--- script/classes/heavy.lua
+++ script/classes/heavy.lua
@@ -1,12 +1,10 @@
 local class =
 {
-  name = class_names.heavy
+  name = class_names.heavy,
+  primary_weapons = 
+  {
+    weapon_names.minigun
+  },
+  secondary_weapons = nil
 }
-local create = function(player)
-  player.create_character(class.name)
-  local character = player.character
-  character.insert("heavy-armor")
-  character.insert("heavy-gun")
-  character.insert("heavy-ammo")
-end
-return setmetatable(class, {__call = function(self, ...) create(...) end})
+return class
