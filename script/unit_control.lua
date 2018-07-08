8091cfb495ba5ba901c5830ee0e37d0e0201ace9
Klonan <konranwoodhouse@hotmail.com>
2018-07-08 22:36:51 +0200

    New unit control system

--- script/unit_control.lua
+++ script/unit_control.lua
@@ -5,10 +5,11 @@ local unit_control = {}
 local data =
 {
   button_action_index = {},
-  selected_groups = {},
+  groups = {},
+  selected_units = {},
   open_frames = {},
-  unit_owners = {},
-  stack_event_check = {}
+  units = {},
+  stack_event_check = {},
 }
 
 --[[on_player_selected_area
@@ -34,7 +35,7 @@ end
 local gui_actions =
 {
   move_button = function(event)
-    if not data.selected_groups[event.player_index] then return end
+    if not data.selected_units[event.player_index] then return end
     local player = game.players[event.player_index]
     if not (player and player.valid) then return end
     clean(player)
@@ -59,9 +60,9 @@ local gui_actions =
     data.stack_event_check[player.index] = {item = names.unit_attack_tool, ignore_tick = game.tick}
   end,
   stop_button = function(event)
-    local group = data.selected_groups[event.player_index]
+    local group = data.selected_units[event.player_index]
     if not (group and group.valid) then
-      data.selected_groups[event.player_index] = nil
+      data.selected_units[event.player_index] = nil
       return
     end
     group.set_command
@@ -72,16 +73,15 @@ local gui_actions =
 }
 
 local make_unit_gui = function(frame, group)
+  if not group then return end
   frame.clear()
   local map = {}
-  local entities = group.members
-  if #entities == 0 then
+  if table_size(group) == 0 then
     util.deregister_gui(frame, data.button_action_index)
     frame.destroy()
-    group.destroy()
     return
   end
-  for k, ent in pairs (group.members) do
+  for unit_number, ent in pairs (group) do
     map[ent.name] = (map[ent.name] or 0) + 1
   end
   local tab = frame.add{type = "table", column_count = 6}
@@ -103,38 +103,46 @@ local make_unit_gui = function(frame, group)
   data.button_action_index[stop.index] = {name = "stop_button"}
 end
 
-local deregister_unit = function(entity, new_group)
+local deregister_unit = function(entity)
   if not (entity and entity.valid) then return end
   if not (entity.type == "unit") then return end
-
-  if new_group and new_group.valid then
-    entity.set_command
-    {
-    type = defines.command.group,
-    distraction = defines.distraction.none,
-    group = new_group
-    }
-  else
-    entity.set_command{type = defines.command.stop}
+  local unit_number = entity.unit_number
+  local unit = data.units[unit_number]
+  if not unit then
+    --game.print("No unit to deregister")
+    return
   end
-
-  local player_index = data.unit_owners[entity.unit_number]
-  if not player_index then return end
+  local group = unit.group
+  if group then
+    --game.print("Deregistered unit from group")
+    group[unit_number] = nil
+  end
+  local player_index = unit.player
+  if not player_index then
+    --game.print("No player index attached to unit info")
+    return
+  end
 
   local frame = data.open_frames[player_index]
-  local group = data.selected_groups[player_index]
   
   if not (frame and frame.valid) then
-    data.selected_groups[player_index] = nil
+    data.selected_units[player_index] = nil
     return
   end
 
-  if not (group and group.valid) then
-    util.deregister_gui(frame, button_action_index)
-    frame.destroy()
-    return
-  end
   make_unit_gui(frame, group)
+end
+
+local get_selected_units = function(player_index)
+  local data = data.selected_units
+  local selected = data[player_index] or {}
+  for unit_number, entity in pairs (selected) do
+    if not entity.valid then
+      selected[unit_number] = nil
+    end
+  end
+  data[player_index] = selected
+  return selected
 end
 
 local unit_selection = function(event)
@@ -147,27 +155,25 @@ local unit_selection = function(event)
   local surface = player.surface
   local area = event.area
   local center = util.center(area)
-  local group = data.selected_groups[player.index]
-  if not (append and group and group.valid) then
-    game.print("Making new unit group")
-    group = surface.create_unit_group{position = center, force = player.force}
-  end
-  data.selected_groups[player.index] = group
   local index = player.index
+  local group = get_selected_units(index)
+  if not append then
+    game.print("New selected units")
+    group = {}
+  end
+  data.selected_units[index] = group
   game.print(#entities)
   for k, ent in pairs (entities) do
-    data.unit_owners[ent.unit_number] = index
-    if ent.unit_group and ent.unit_group ~= group then
-      deregister_unit(ent, group)
-    end
-    group.add_member(ent)
-    ent.set_command{
-      type = defines.command.group,
-      distraction = defines.distraction.none,
-      group = group
+    deregister_unit(ent)
+    group[ent.unit_number] = ent
+    data.units[ent.unit_number] = 
+    {
+      entity = ent,
+      player = index,
+      group = group,
+      command = {}
     }
   end
-  game.print(#group.members)
   local gui = player.gui.left
   local old_frame = data.open_frames[player.index]
   if (old_frame and old_frame.valid) then
@@ -182,65 +188,109 @@ local unit_selection = function(event)
 end
 
 local move_units = function(event)
-  local group = data.selected_groups[event.player_index]
-  if not (group and group.valid) then
-    data.selected_groups[event.player_index] = nil
+  local group = get_selected_units(event.player_index)
+  if not group then
+    data.selected_units[event.player_index] = nil
     return
   end
   local position = util.center(event.area)
-  group.set_command
-  {
-    type = defines.command.go_to_location,
-    destination = position,
-    distraction = defines.distraction.none
-  }
+  local offset = 1.5
+  local radius = math.ceil((offset * table_size(group) ^ 0.5)/2)
+  local index = 1
+  local destinations = {}
+  for x = -radius, radius, offset do
+    for y = -radius, radius, offset do
+      destinations[index] = {position.x + x, position.y + y}
+      index = index + 1
+    end
+  end
+
+
+  local command = function(i) return
+    {
+      type = defines.command.go_to_location,
+      destination = destinations[i],
+      distraction = defines.distraction.none
+    }
+  end
+
+  index = 1
+  for unit_number, entity in pairs (group) do
+    entity.set_command(command(index))
+    index = index + 1
+  end
   game.players[event.player_index].play_sound({path = names.unit_move_sound})
 end
 
 local attack_move_units = function(event)
-  local group = data.selected_groups[event.player_index]
-  if not (group and group.valid) then
-    data.selected_groups[event.player_index] = nil
+  local group = get_selected_units(event.player_index)
+  if not group then
+    data.selected_units[event.player_index] = nil
     return
   end
   local position = util.center(event.area)
-  group.set_command
-  {
-    type = defines.command.go_to_location,
-    destination = position,
-    distraction = defines.distraction.by_anything
-  }
+  local offset = 1.5
+  local radius = math.ceil((offset * table_size(group) ^ 0.5)/2)
+  local index = 1
+  local destinations = {}
+  for x = -radius, radius, offset do
+    for y = -radius, radius, offset do
+      destinations[index] = {position.x + x, position.y + y}
+      index = index + 1
+    end
+  end
+
+
+  local command = function(i) return
+    {
+      type = defines.command.go_to_location,
+      destination = destinations[i],
+      distraction = defines.distraction.by_anything
+    }
+  end
+
+  index = 1
+  for unit_number, entity in pairs (group) do
+    entity.set_command(command(index))
+    index = index + 1
+  end
   game.players[event.player_index].play_sound({path = names.unit_move_sound})
 end
 
 local attack_units = function(event)
-  local entities = event.entities
-  if #entities == 0 then return end
-  local group = data.selected_groups[event.player_index]
-  if not (group and group.valid) then
-    data.selected_groups[event.player_index] = nil
+  local group = get_selected_units(event.player_index)
+  if not group then
+    data.selected_units[event.player_index] = nil
     return
   end
-  if #entities == 1 and entities[1].valid then
-    group.set_command
+  local entities = event.entities
+  if #entities == 0 then return end
+  local positions = {}
+  for k, ent in pairs (entities) do
+    positions[ent.position] = ent
+  end
+  local dist = util.distance
+  local attack_closest = function(unit)
+    local closest
+    local min = 5000000
+    local position = unit.position
+    for other_position, ent in pairs (positions) do
+      local sep = dist(position, other_position)
+      if sep < min then
+        min = sep
+        closest = ent
+      end
+    end
+    unit.set_command
     {
       type = defines.command.attack,
       distraction = defines.distraction.none,
-      target = entities[1]
+      target = closest
     }
   end
-  local position = util.center(event.area)
-  local first = entities[1]
-  local last = entities[#entities]
-  local center = {x = (first.position.x + last.position.x) / 2, y = (first.position.y + last.position.y) / 2}
-  local radius = util.distance(center, first.position)
-  group.set_command
-  {
-    type = defines.command.attack_area,
-    destination = center,
-    radius = radius,
-    distaction = defines.distraction.none
-  }
+  for unit_number, unit in pairs (group) do
+    attack_closest(unit)
+  end
   game.players[event.player_index].play_sound({path = names.unit_move_sound})
 end
 
@@ -305,6 +355,19 @@ local on_player_cursor_stack_changed = function(event)
   data.stack_event_check[player.index] = nil
 end
 
+local on_ai_command_completed = function(event)
+  --game.print("Command complete")
+  local unit = data.units[event.unit_number]
+  if unit then
+    --game.print("A matey of our finished a command")
+    local entity = unit.entity
+    if (entity and entity.valid) then
+      entity.set_command{type = defines.command.stop}
+      --game.print("And he has stopped.")
+    end
+  end
+end
+
 local events =
 {
   [defines.events.on_player_selected_area] = on_player_selected_area,
@@ -313,6 +376,7 @@ local events =
   [defines.events.on_gui_click] = on_gui_click,
   [defines.events.on_entity_died] = on_entity_died,
   [defines.events.on_player_cursor_stack_changed] = on_player_cursor_stack_changed,
+  [defines.events.on_ai_command_completed] = on_ai_command_completed,
   --[defines.event.on_player_created] = on_player_created
   [defines.events[require("shared").hotkeys.unit_move]] = gui_actions.move_button
 }
@@ -321,9 +385,13 @@ unit_control.on_event = handler(events)
 
 unit_control.on_init = function()
   global.unit_control = data
-  game.map_settings.path_finder.max_steps_worked_per_tick = 100000
+  game.map_settings.path_finder.max_steps_worked_per_tick = 1000000
   game.map_settings.path_finder.start_to_goal_cost_multiplier_to_terminate_path_find = 100000
   game.map_settings.path_finder.short_request_max_steps = 5000
+  game.map_settings.path_finder.min_steps_to_check_path_find_termination = 50000
+  game.map_settings.path_finder.max_clients_to_accept_any_new_request = 50000
+  game.map_settings.steering.moving.force_unit_fuzzy_goto_behavior = true
+  
 end
 
 unit_control.on_load = function()
