14c5863b254de566167085432c4b2d9afcab972f
Klonan <konranwoodhouse@hotmail.com>
2018-07-31 19:02:54 +0200

    Remove recon outpost

--- data/entities/entities.lua
+++ data/entities/entities.lua
@@ -1,6 +1,5 @@
 local require = function(name) return require("data/entities/"..name) end
 
-require("recon_outpost/recon_outpost")
 require ("teleporters/teleporters")
 require ("command_center/command_center")
 require ("deploy_machine/deploy_machine")
