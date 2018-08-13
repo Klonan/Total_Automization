62baab35e5084f07b401b1a95872d0e2dca1e5fc
Klonan <konranwoodhouse@hotmail.com>
2018-08-13 19:03:17 +0200

    Activate and fix killcam

--- script/killcam.lua
+++ script/killcam.lua
@@ -11,13 +11,15 @@ local make_killcam = function(player, cause)
   local name = cause.name
   if cause.type == "player" and cause.player then
     name = cause.player.name
+  else
+    name = game.entity_prototypes[name].localised_name
   end
-  local frame = gui.add{type = "frame", caption = "You were killed by "..name}
+  local frame = gui.add{type = "frame", caption = {"", "You were killed by ", name}}
   local camera = frame.add{type = "camera", position = cause.position, zoom = 1}
   camera.style.width = player.display_resolution.width * 0.8
   camera.style.height = player.display_resolution.height * 0.8
   local cams = data.killcams
-  cams[player.index] = {gui = camera, cause = cause}
+  cams[player.index] = {gui = camera, cause = cause, frame = frame}
 end
 
 local on_player_died = function(event)
@@ -32,7 +34,7 @@ local on_tick = function(event)
   for k, cam in pairs (cams) do
     local player = game.players[k]
     if player.character then
-      cam.gui.destroy()
+      cam.frame.destroy()
       cams[k] = nil
     elseif cam.cause.valid then
       cam.gui.position = cam.cause.position
