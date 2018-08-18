7f97cc3550d6ae1cc4fd73f1417f420621e161da
Klonan <konranwoodhouse@hotmail.com>
2018-08-18 23:38:18 +0200

    class gui v2 WIP, getting there

--- script/classes/classes.lua
+++ script/classes/classes.lua
@@ -40,69 +40,11 @@ local stats =
 local choose_class_gui_init
 local gui_functions = 
 {
-  class_button_action = function(event, param)
-    local element = event.element
-    local data = param.data
-    local name = data.name
-    local player = game.players[event.player_index]
-    local flow = param.class_flow
-    flow.clear()
-    local inner = flow.add{type = "flow", direction = "horizontal"}
-    inner.style.vertically_squashable = true
-    --local sprite = inner.add{type = "sprite", sprite = data.name}
-    local character = game.entity_prototypes[data.name]
-    local inner_more = inner.add{type = "flow", direction = "vertical"}
-    local info = inner_more.add{type = "frame", caption = data.name}
-    local stat_table = info.add{type = "table", column_count = 2}
-    for stat, name in pairs (stats) do
-      stat_table.add{type = "label", caption = name..": "}
-      local value = character[stat]
-      if value < 1 then
-        stat_table.add{type = "label", caption = value * 100}
-      else
-        stat_table.add{type = "label", caption = value}
-      end
-    end
-    local loadout = inner_more.add{type = "frame", caption = "Loadout"}
-    local loadout_table = loadout.add{type = "table", column_count = 2}
-    loadout_table.add{type = "label", caption = "Primary: "}
-    local primaries = loadout_table.add{type = "drop-down"}
-    local set = false
-    for k, name in pairs (data.primary_weapons) do
-      primaries.add_item(name)
-      if player.get_item_count(name) > 0 then
-        primaries.selected_index = k
-        set = true
-      end
-    end
-    if not set then
-      primaries.selected_index = 1
-    end
-
-    loadout_table.add{type = "label", caption = "Secondary: "}
-    local secondaries = loadout_table.add{type = "drop-down"}
-    set = false
-    for k, name in pairs (data.secondary_weapons) do
-      secondaries.add_item(name)
-      if player.get_item_count(name) > 0 then
-        secondaries.selected_index = k
-        set = true
-      end
-    end
-    if not set then
-      secondaries.selected_index = 1
-    end
-
-    local confirm_button = inner_more.add{type = "button", caption = "GO GO GO"}
-    data.elements[confirm_button.index] = {name = "confirm_button_action", param = {data = data, primaries = primaries, secondaries = secondaries, top_frame = param.top_frame}}
-  end,
-  confirm_button_action = function(event, param)
+  confirm_loadout = function(event, param)
     local element = event.element
     local player = game.players[event.player_index]
-    local data = param.data
-    set_class(player, data.name, data.primary_weapons[param.primaries.selected_index], data.secondary_weapons[param.secondaries.selected_index])
-    util.deregister_gui(param.top_frame, data.elements)
-    param.top_frame.destroy()
+    util.deregister_gui(param.gui, data.elements)
+    param.gui.destroy()
   end,
   close_gui = function(event, param)
     util.deregister_gui(param.gui, data.elements)
@@ -129,6 +71,34 @@ local gui_functions =
     data.selected_loadouts[player.name].primary_ammo = listbox.get_item(listbox.selected_index)
     choose_class_gui_init(player)
   end,
+  change_selected_secondary_weapon = function(event, param)
+    local listbox = event.element
+    if not (listbox and listbox.valid) then return end
+    local player = game.players[event.player_index]
+    data.selected_loadouts[player.name].secondary_weapon = listbox.get_item(listbox.selected_index)
+    choose_class_gui_init(player)
+  end,
+  change_selected_secondary_ammo = function(event, param)
+    local listbox = event.element
+    if not (listbox and listbox.valid) then return end
+    local player = game.players[event.player_index]
+    data.selected_loadouts[player.name].secondary_ammo = listbox.get_item(listbox.selected_index)
+    choose_class_gui_init(player)
+  end,
+  change_selected_pistol_weapon = function(event, param)
+    local listbox = event.element
+    if not (listbox and listbox.valid) then return end
+    local player = game.players[event.player_index]
+    data.selected_loadouts[player.name].pistol_weapon = listbox.get_item(listbox.selected_index)
+    choose_class_gui_init(player)
+  end,
+  change_selected_pistol_ammo = function(event, param)
+    local listbox = event.element
+    if not (listbox and listbox.valid) then return end
+    local player = game.players[event.player_index]
+    data.selected_loadouts[player.name].pistol_ammo = listbox.get_item(listbox.selected_index)
+    choose_class_gui_init(player)
+  end,
 }
 
 choose_class_gui_init = function(player)
@@ -136,21 +106,21 @@ choose_class_gui_init = function(player)
   local gui = player.gui.center
   util.deregister_gui(gui, data.elements)
   gui.clear()
-  local frame = gui.add{type = "frame", direction = "horizontal", caption = "CHOOSE YOUR LOADOUT"}
-  player.opened = frame
-  data.elements[frame.index] = {name = "close_gui", param = {gui = frame}}
+  local frame = gui.add{type = "frame", direction = "vertical", caption = "CHOOSE YOUR LOADOUT"}
   frame.style = "image_frame"
   frame.style.width = player.display_resolution.width
   frame.style.height = player.display_resolution.height
-  frame.style.top_padding = 32
-  frame.style.bottom_padding = 32
-  frame.style.left_padding = 32
-  frame.style.right_padding = 32
+  frame.style.top_padding = 16
+  frame.style.bottom_padding = 16
+  frame.style.left_padding = 16
+  frame.style.right_padding = 16
   frame.style.align = "center"
   frame.style.vertical_align = "top"
-  local class_frame = frame.add{type = "frame", caption = "Choose your class", direction = "vertical"}
-  class_frame.style.vertically_stretchable = false
-  local loadout_listbox = class_frame.add{type = "list-box"}
+  local mid_flow = frame.add{type = "flow", direction = "horizontal"}
+  
+  local loadout_frame = mid_flow.add{type = "frame", caption = "Choose your class", direction = "vertical"}
+  loadout_frame.style.vertically_stretchable = false
+  local loadout_listbox = loadout_frame.add{type = "list-box"}
   loadout_listbox.style.horizontally_stretchable = true
   data.elements[loadout_listbox.index] = {name = "change_selected_class"}
 
@@ -167,7 +137,7 @@ choose_class_gui_init = function(player)
   end
   loadout_listbox.selected_index = index
 
-  local info_table = class_frame.add{type = "table", column_count = 5, style = "slot_table"}
+  local info_table = loadout_frame.add{type = "table", column_count = 5, style = "slot_table"}
   local equipments = game.equipment_prototypes
   for name, count in pairs (loadouts[selected_loadout.name].equipment) do
     local equipment = equipments[name]
@@ -176,7 +146,7 @@ choose_class_gui_init = function(player)
     end
   end
   
-  local primary_gun_frame = frame.add{type = "frame", caption = "Choose your Primary weapon", direction = "vertical"}
+  local primary_gun_frame = mid_flow.add{type = "frame", caption = "Choose your Primary weapon", direction = "vertical"}
   primary_gun_frame.style.vertically_stretchable = false
   local primary_gun_list = primary_gun_frame.add{type = "list-box"}
   data.elements[primary_gun_list.index] = {name = "change_selected_primary_weapon"}
@@ -195,7 +165,7 @@ choose_class_gui_init = function(player)
   primary_gun_list.style.vertically_squashable = true
   primary_gun_list.style.horizontally_stretchable = true
 
-  primary_gun_frame.add{type = "label", caption = "Primary Ammo"}
+  primary_gun_frame.add{type = "label", caption = "Choose Primary Ammo", style = "description_label"}
   local primary_ammo_list = primary_gun_frame.add{type = "list-box"}
   data.elements[primary_ammo_list.index] = {name = "change_selected_primary_ammo"}
   primary_ammo_list.style.horizontally_stretchable = true
@@ -210,6 +180,94 @@ choose_class_gui_init = function(player)
   primary_ammo_list.selected_index = index
   primary_ammo_list.style.vertically_squashable = true
   primary_ammo_list.style.horizontally_stretchable = true
+  
+  local secondary_gun_frame = mid_flow.add{type = "frame", caption = "Choose your Secondary weapon", direction = "vertical"}
+  secondary_gun_frame.style.vertically_stretchable = false
+  local secondary_gun_list = secondary_gun_frame.add{type = "list-box"}
+  data.elements[secondary_gun_list.index] = {name = "change_selected_secondary_weapon"}
+  local count = 1
+  local index = 1
+  local selected_secondary = util.first_key(selected_specification.secondary_weapons)
+  for name, ammos in pairs (selected_specification.secondary_weapons) do
+    secondary_gun_list.add_item(name)
+    if name == selected_loadout.secondary_weapon then
+      index = count
+      selected_secondary = name
+    end
+    count = count + 1
+  end
+  secondary_gun_list.selected_index = index
+  secondary_gun_list.style.vertically_squashable = true
+  secondary_gun_list.style.horizontally_stretchable = true
+
+  secondary_gun_frame.add{type = "label", caption = "Choose Secondary Ammo", style = "description_label"}
+  local secondary_ammo_list = secondary_gun_frame.add{type = "list-box"}
+  data.elements[secondary_ammo_list.index] = {name = "change_selected_secondary_ammo"}
+  secondary_ammo_list.style.horizontally_stretchable = true
+  local index = 1
+  for k, ammo in pairs (selected_specification.secondary_weapons[selected_secondary]) do
+    secondary_ammo_list.add_item(ammo)
+    if ammo == selected_loadout.secondary_ammo then
+      index = k
+    end
+    count = count + 1
+  end
+  secondary_ammo_list.selected_index = index
+  secondary_ammo_list.style.vertically_squashable = true
+  secondary_ammo_list.style.horizontally_stretchable = true
+
+    
+  local pistol_gun_frame = mid_flow.add{type = "frame", caption = "Choose your pistol", direction = "vertical"}
+  pistol_gun_frame.style.vertically_stretchable = false
+  local pistol_gun_list = pistol_gun_frame.add{type = "list-box"}
+  data.elements[pistol_gun_list.index] = {name = "change_selected_pistol_weapon"}
+  local count = 1
+  local index = 1
+  local selected_pistol = util.first_key(selected_specification.pistol_weapons)
+  for name, ammos in pairs (selected_specification.pistol_weapons) do
+    pistol_gun_list.add_item(name)
+    if name == selected_loadout.pistol_weapon then
+      index = count
+      selected_pistol = name
+    end
+    count = count + 1
+  end
+  pistol_gun_list.selected_index = index
+  pistol_gun_list.style.vertically_squashable = true
+  pistol_gun_list.style.horizontally_stretchable = true
+
+  pistol_gun_frame.add{type = "label", caption = "Choose Pistol Ammo", style = "description_label"}
+  local pistol_ammo_list = pistol_gun_frame.add{type = "list-box"}
+  data.elements[pistol_ammo_list.index] = {name = "change_selected_pistol_ammo"}
+  local index = 1
+  for k, ammo in pairs (selected_specification.pistol_weapons[selected_pistol]) do
+    pistol_ammo_list.add_item(ammo)
+    if ammo == selected_loadout.pistol_ammo then
+      index = k
+    end
+    count = count + 1
+  end
+  pistol_ammo_list.selected_index = index
+  pistol_ammo_list.style.vertically_squashable = true
+  pistol_ammo_list.style.horizontally_stretchable = true
+
+  local final_button_flow = frame.add{type = "flow"}
+  final_button_flow.style.horizontally_stretchable = true
+  final_button_flow.style.vertically_stretchable = true
+  --final_button_flow.style.align = "right"
+  final_button_flow.style.vertical_align = "bottom"
+
+  if player.character then
+    player.opened = frame
+    data.elements[frame.index] = {name = "close_gui", gui = frame}
+    local back_button = final_button_flow.add{type = "button", style = "back_button", caption = "Cancel"}
+    data.elements[back_button.index] = {name = "close_gui", gui = frame}
+  end
+
+  local push = final_button_flow.add{type = "flow"}
+  push.style.horizontally_stretchable = true
+  local go_button = final_button_flow.add{type = "button", style = "confirm_button", caption = "Confirm loadout selection"}
+  data.elements[go_button.index] = {name = "confirm_loadout", gui = frame}
 
 end
 
@@ -233,7 +291,7 @@ local on_gui_interaction = function(event)
 
   local action = data.elements[element.index]
   if action then
-    gui_functions[action.name](event, action.param)
+    gui_functions[action.name](event, action)
   end
 end
 
@@ -244,7 +302,7 @@ local on_gui_closed = function(event)
   --I need to think about this, might be hassle or good system...
   local action = data.elements[gui.index]
   if action then
-    gui_functions[action.name](event, action.param)
+    gui_functions[action.name](event, action)
   end
 end
 
