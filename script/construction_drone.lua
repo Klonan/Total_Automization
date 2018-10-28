9710905cfd590fac78327dbc8cabe045c91e9179
Klonan <11986037+Klonan@users.noreply.github.com>
2018-10-28 18:56:23 +0100

    Tabs to spaces?

--- script/construction_drone.lua
+++ script/construction_drone.lua
@@ -1,83 +1,83 @@
 local max = math.huge
 
 local dist = function(cell_a, cell_b)
-	local position1 = cell_a.owner.position
-	local position2 = cell_b.owner.position
-	return ((position2.x - position1.x) * (position2.x - position1.x)) + ((position2.y - position1.y) * (position2.y - position1.y))
+  local position1 = cell_a.owner.position
+  local position2 = cell_b.owner.position
+  return ((position2.x - position1.x) * (position2.x - position1.x)) + ((position2.y - position1.y) * (position2.y - position1.y))
 end
 
 local lowest_f_score = function(set, f_score)
   local lowest = max
   local bestcell
-	for k, cell in pairs(set) do
-		local score = f_score[cell.owner.unit_number]
-		if score <= lowest then
+  for k, cell in pairs(set) do
+    local score = f_score[cell.owner.unit_number]
+    if score <= lowest then
       lowest = score 
       bestcell = cell
-		end
-	end
-	return bestcell
+    end
+  end
+  return bestcell
 end
 
 local insert = table.insert
 local unwind_path
 unwind_path = function(flat_path, map, current_cell)
-	local index = current_cell.owner.unit_number
-	if map[index] then
-		insert(flat_path, 1, map[index])
-		return unwind_path(flat_path, map, map[index])
-	else
-		return flat_path
-	end
+  local index = current_cell.owner.unit_number
+  if map[index] then
+    insert(flat_path, 1, map[index])
+    return unwind_path(flat_path, map, map[index])
+  else
+    return flat_path
+  end
 end
 
 local get_path = function(start, goal, cells)
-	
-	local closed_set = {}
-	local open_set = {}
-	local came_from = {}
-	
-	local g_score = {}
-	local f_score = {}
-	local start_index = start.owner.unit_number
-	open_set[start_index] = start
-	g_score[start_index] = 0
-	f_score[start_index] = dist(start, goal)
-
-	local insert = table.insert
-	while table_size(open_set) > 0 do
-
-		local current = lowest_f_score(open_set, f_score)
-
-		if current == goal then
-			local path = unwind_path({}, came_from, goal)
-			insert(path, goal)
-			return path
-		end
-
-		local current_index = current.owner.unit_number
-		open_set[current_index] = nil
-		closed_set[current_index] = current
-
-		for k, neighbor in pairs(current.neighbours) do
-			local neighbor_index = neighbor.owner.unit_number
-			if not closed_set[neighbor_index] then
-				local tentative_g_score = g_score[current_index] + dist(current, neighbor)
-				local new_node = not open_set[neighbor_index]
-				if new_node then
-					open_set[neighbor.owner.unit_number] = neighbor
-					f_score[neighbor.owner.unit_number] = max
-				end
-				if new_node or tentative_g_score < g_score[neighbor_index] then
-					came_from[neighbor_index] = current
-					g_score[neighbor_index] = tentative_g_score
-					f_score[neighbor_index] = g_score[neighbor_index] + dist(neighbor, goal)
-				end
-			end
-		end
-
-	end
-	return nil -- no valid path
+  
+  local closed_set = {}
+  local open_set = {}
+  local came_from = {}
+  
+  local g_score = {}
+  local f_score = {}
+  local start_index = start.owner.unit_number
+  open_set[start_index] = start
+  g_score[start_index] = 0
+  f_score[start_index] = dist(start, goal)
+  
+  local insert = table.insert
+  while table_size(open_set) > 0 do
+    
+    local current = lowest_f_score(open_set, f_score)
+    
+    if current == goal then
+      local path = unwind_path({}, came_from, goal)
+      insert(path, goal)
+      return path
+    end
+    
+    local current_index = current.owner.unit_number
+    open_set[current_index] = nil
+    closed_set[current_index] = current
+    
+    for k, neighbor in pairs(current.neighbours) do
+      local neighbor_index = neighbor.owner.unit_number
+      if not closed_set[neighbor_index] then
+        local tentative_g_score = g_score[current_index] + dist(current, neighbor)
+        local new_node = not open_set[neighbor_index]
+        if new_node then
+          open_set[neighbor.owner.unit_number] = neighbor
+          f_score[neighbor.owner.unit_number] = max
+        end
+        if new_node or tentative_g_score < g_score[neighbor_index] then
+          came_from[neighbor_index] = current
+          g_score[neighbor_index] = tentative_g_score
+          f_score[neighbor_index] = g_score[neighbor_index] + dist(neighbor, goal)
+        end
+      end
+    end
+    
+  end
+  return nil -- no valid path
 end
 
 local get_closest_cell = function(entity, cells)
@@ -90,7 +90,7 @@ local get_closest_cell = function(entity, cells)
       entities[k] = cell.owner
     end
   else
-     entities = surface.find_entities_filtered{type = "roboport", force = force}
+    entities = surface.find_entities_filtered{type = "roboport", force = force}
   end
   local closest = surface.get_closest(entity.position, entities)
   if closest then
@@ -124,7 +124,7 @@ local set_drone_command = function(unit, destination_entity)
   if not (starting_cell and destination_cell) then return end
   local path = get_path(starting_cell, destination_cell, cells)
   if not path then game.print("No path for drone command") end
-
+  
   local biter = unit.surface.create_entity{name = "SMG Guy", force = unit.force, position = {unit.position.x + 3, unit.position.y}}
   local commands = {}
   for k, cell in pairs (path) do
@@ -148,7 +148,6 @@ local set_drone_command = function(unit, destination_entity)
     commands = commands,
     structure_type = defines.compound_command.return_last
   }
-
 
 end
 
